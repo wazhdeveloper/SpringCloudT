00dbeaeab58caa98663c06e8f7072c9a1073601a
峰王 <wang13770583001@163.com>
2022-09-24 18:46:08 +0800

    springcloud-alibaba -> nacos commit2

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/springcloud/controller/PaymentNacosController.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @author wzhstart
+ * @creat 2022-09-24-18:42
+ */
+@RestController
+public class PaymentNacosController {
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Value("${service-url.nacos-user-service}")
+    private String serverURL;
+
+    @GetMapping("/consumer/payment/nacos/{id}")
+    public String paymentInfo(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(serverURL + "/payment/nacos/" + id, String.class);
+    }
+
+}
